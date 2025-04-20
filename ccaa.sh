c397f4767159fc870bc20b3ee664d51a1c867494
wfq1971 <90599179+wfq1971@users.noreply.github.com>
2025-04-20 16:18:45 +0800

    Update ccaa.sh

--- ccaa.sh
+++ ccaa.sh
@@ -12,7 +12,7 @@ if [ $1 = 'cdn' ]
 	then
 	aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.36.0/aria2-1.36.0-linux-gnu-64bit-build1.tar.bz2'
 	filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.32.0/linux-amd64-filebrowser.tar.gz'
-	master_url='https://github.com/helloxz/ccaa/archive/master.zip'
+	master_url='https://github.com/wfq1971/ccaa/archive/master.zip'
 	ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
 	else
 	aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
